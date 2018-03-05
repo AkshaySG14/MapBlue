6c4f42ca5a9d4cc44fa211f374fc67024be32cf3
akshay <akshaysg@umich.edu>
2018-03-05 14:49:15 -0500

    Initial Commit

---
+++ MapBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapBlue
+//
+//  Created by Akshay Subramaniam on 5/3/18.
+//  Copyright © 2018 MediocreAtBest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
