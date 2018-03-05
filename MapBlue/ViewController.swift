4534682e21c711431216dc353bdea1eeb95676e5
akshay <akshaysg@umich.edu>
2018-03-05 16:33:02 -0500

    Hid nav bar

--- MapBlue/ViewController.swift
+++ MapBlue/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+
     }
 
     override func didReceiveMemoryWarning() {
