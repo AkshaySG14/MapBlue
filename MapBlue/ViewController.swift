7d0aec5aaf9d7144e4d4bb740941ad812df38e62
akshay <akshaysg@umich.edu>
2018-03-06 22:36:15 -0500

    Made further changes to UI and initial screen.

--- MapBlue/ViewController.swift
+++ MapBlue/ViewController.swift
@@ -9,19 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var EECSButton: UIButton!
+    let borderAlpha : CGFloat = 0.7
+    let cornerRadius : CGFloat = 5.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Hide the navigation bar on the this view controller
         self.navigationController?.setNavigationBarHidden(true, animated: false)
-
+        self.EECSButton.backgroundColor = UIColor.clear
+        self.EECSButton.layer.borderWidth = 1.0
+        self.EECSButton.layer.borderColor = UIColor(white: 1.0, alpha: borderAlpha).cgColor
+        self.EECSButton.layer.cornerRadius = cornerRadius
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+class BuildingViewController : ViewController {
+    
 }
 
